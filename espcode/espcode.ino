60131b43c28e6b29915083143fbda0ce9792310c
Yasith Silva <133279208+yasith46@users.noreply.github.com>
2024-05-04 17:11:06 +0530

    Added Time, Menus, and Send
    Added timezones, NTP, added settings and other menus (display only), made changes to wifiLoop (callPress)

--- espcode/espcode.ino
+++ espcode/espcode.ino
@@ -4,24 +4,87 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_ST7735.h>
 #include <SPI.h>
+#include <time.h>
 
-#define cs 10
-#define dc 9
-#define rst 8
-#define UP 12
-#define DOWN 14
-#define SELOK 15
+#define cs    5
+#define dc    17
+#define rst   22
+#define UP    21
+#define DOWN  13
+#define SELOK 14
+#define CALL1 33
+#define CALL2 34
+#define CALL3 35
+#define IND1  25
+#define IND2  26
+#define IND3  27
 
-int spacing = 3;  // Linespacing for the display
+#define NTP_SERVER     "pool.ntp.org"
+#define UTC_OFFSET_DST 0
+int UTC_OFFSET = 0;
+
+int curr_hour = 0;
+int curr_mins = 0;
+
+const String console_id = "0102401";
+
+int spacing         = 3;  // Linespacing for the display
 int default_spacing = 3;
+int refresh_rate    = 500;
 
 char path[] = "/";
 char host[] = "192.168.40.183"; // change ip to the server ////
+
+String calls[] = {"000","001","101"}; 
 
 WebSocketClient webSocketClient;
 WiFiClient client;
 
 Adafruit_ST7735 disp = Adafruit_ST7735(cs, dc, rst);
+
+
+
+// ------------------------------ Functions for displaying ------------------------------ 
+
+void disp_write(String distring, int size, int x, int line, int color = 0xFFFF) {
+  // ------------------------------ 
+  // Prints onto display
+  // ------------------------------ 
+
+  int y = size*10*(line-1)+spacing;
+  //static_assert(y <= 128, "Display overflow");
+
+  disp.setTextColor(color);
+  disp.setTextSize(size);
+  disp.setCursor(x,y);
+  disp.print(distring);
+}
+
+void disp_start() {
+  // ------------------------------ 
+  // Opening Animation
+  // ------------------------------ 
+
+  int fadetime = 260;
+  for (uint16_t color=0; color<=0xFFFF; color+=0x210) {
+    disp_write("Andon", 5, 5, 2, color);
+    delay(fadetime/5);
+  }
+  delay(1000-fadetime);
+  disp_cls();
+}
+
+
+
+void disp_cls() {
+  // ------------------------------
+  // Clears Display
+  // ------------------------------ 
+
+  disp.fillScreen(ST7735_BLACK);
+}
+
+
 
 void wifiSetup(){ //WiFiManager, Local intialization. Once its business is done, there is no need to keep it around
   WiFiManager wm;
@@ -73,8 +136,9 @@ void wifiSetup(){ //WiFiManager, Local intialization. Once its business is done,
 }
 
 
+
 void wifiLoop(){
- String data;
+  String data;
 
   if (client.connected()) {
     
@@ -83,7 +147,7 @@ void wifiLoop(){
       Serial.print("Received data: ");
       Serial.println(data);
     }
-   
+
     String dataArray = "[1,2,3]";
     
     webSocketClient.sendData(dataArray);
@@ -99,65 +163,78 @@ void wifiLoop(){
   delay(3000);
 }
 
-// ------------------------------ Functions for displaying ------------------------------ 
 
-void disp_write(String distring, int size, int x, int line, int color = 0xFFFF) {
-  // ------------------------------ 
-  // Prints onto display
-  // ------------------------------ 
 
-  int y = size*10*(line-1)+spacing;
-  //static_assert(y <= 128, "Display overflow");
+void callPress(){
+  // ---------------------------------
+  // Sending data when the button is pressed
+  // ---------------------------------
 
-  disp.setTextColor(color);
-  disp.setTextSize(size);
-  disp.setCursor(x,y);
-  disp.print(distring);
-}
-
-void disp_start() {
-  // ------------------------------ 
-  // Opening Animation
-  // ------------------------------ 
-
-  int fadetime = 260;
-  for (uint16_t color=0; color<=0xFFFF; color+=0x210) {
-    disp_write("Andon", 5, 5, 2, color);
-    delay(fadetime/5);
+  if (client.connected()) {
+    if (digitalRead(CALL1)) {
+      updateTime();
+      webSocketClient.sendData("[" + String(curr_hour) + "," + String(curr_mins) + "," + console_id + "," + calls[0] + "]");
+    } else if (digitalRead(CALL2)) {
+      updateTime();
+      webSocketClient.sendData("[" + String(curr_hour) + "," + String(curr_mins) + "," + console_id + "," + calls[1] + "]");
+    } else if (digitalRead(CALL3)) {
+      updateTime();
+      webSocketClient.sendData("[" + String(curr_hour) + "," + String(curr_mins) + "," + console_id + "," + calls[2] + "]");
+    }
+        
+  } else {
+    Serial.println("Client disconnected.");
+    while (1) {
+      // Hang on disconnect.
+    }
   }
-  delay(1000-fadetime);
-  disp_cls();
+  
+  // wait to fully let the client disconnect
+  delay(3000);
 }
 
-void disp_cls() {
-  // ------------------------------
-  // Clears Display
-  // ------------------------------ 
 
-  disp.fillScreen(ST7735_BLACK);
+
+void updateTime() {
+  // -----
+  // Getting the time from the NTP server
+  // -----
+
+  struct tm timeinfo;
+
+  // Check if there is any error in connection
+  if (!getLocalTime(&timeinfo)) {
+    disp_write("Connection Error",0,5,1);
+    delay(200);
+    disp_cls();
+    return;
+  }
+
+  curr_hour   = timeinfo.tm_hour;
+  curr_mins = timeinfo.tm_min;
 }
 
-String* getStatus() {
-  // ------------------------------ 
-  // Gets the statuses
-  // ------------------------------ 
 
-  String* statuses = new String[3];
+
+void getCalls(String & call1, String & call2, String & call3) {
+  // ------------------------------ 
+  // Gets the statuses                                                                    ***** EDIT THIS *****
+  // ------------------------------ 
 
   //test
   bool esSet = true;
   if (esSet) {
-    statuses[0] = "Status 1";
-    statuses[1] = "Status 2";
-    statuses[2] = "Status 3";
+    call1 = "Status 1";
+    call2 = "Status 2";
+    call3 = "Status 3";
   } else {
-    for (int i=0; i<3; i++) {
-      statuses[i] = "";
-    }
+    call1 = "";
+    call2 = "";
+    call3 = "";
   }
-
-  return statuses;
 }
+
+
 
 int checkButtonPress() {
   // ------------------------------ 
@@ -175,20 +252,22 @@ int checkButtonPress() {
   }
 }
 
-void mainmenu(){          // Main Menu
-  spacing = default_spacing;
-  String* statuses = getStatus();
 
-  bool statset = (!statuses[0].isEmpty() || !statuses[1].isEmpty() || !statuses[2].isEmpty()); 
+
+void showMainMenu(){      // Main Menu
+  spacing = default_spacing;
+  getCalls(calls[0], calls[1], calls[2]);
+
+  bool statset = (!calls[0].isEmpty() || !calls[1].isEmpty() || !calls[2].isEmpty()); 
 
   if (!statset) {         // If no statuses set 
-    disp_write("  Set Status..", 2, 5, 1);
+    disp_write("  Do the initial Setup..", 2, 5, 1);
     disp_write("> Settings (Press OK)", 2, 5, 2);
 
   } else {                // If statuses set
-    disp_write(statuses[0], 2, 10, 1);
-    disp_write(statuses[1], 2, 10, 2);
-    disp_write(statuses[2], 2, 10, 3);
+    disp_write(calls[0], 2, 10, 1);
+    disp_write(calls[1], 2, 10, 2);
+    disp_write(calls[2], 2, 10, 3);
   }
 
   if (checkButtonPress()==2) {
@@ -197,9 +276,139 @@ void mainmenu(){          // Main Menu
   }
 }
 
-void settings() {
+
+
+void showSettings(int menu_item) {
+  disp_write(" Connect to WiFi", 2, 5, 1);
+  disp_write(" Choose Calls", 2, 5, 2);
+  disp_write(" Set Timezone", 2, 5, 3);
+  disp_write(" Exit", 2, 5, 4);
+
+  // Cursor
+  switch (menu_item) {
+    case 1:
+      disp_write(">",2,5,1);
+    case 2:
+      disp_write(">",2,5,2);
+    case 3:
+      disp_write(">",2,5,3);
+    case 4:
+      disp_write(">",2,5,4);
+  }
+}
+
+
+
+void connectWiFi() {
 
 }
+
+
+
+void chooseCalls() {
+
+}
+
+
+
+void showTimezone(String timezone_name, int menu_item) {
+  // -----
+  // Displaying the menu for setting timezone
+  // -----
+
+  // Menu items
+  disp_write("  Timezone  "+timezone_name, 2,5,1);
+  disp_write("  Save", 2,5,2);
+
+  // Cursor
+  if (menu_item == 1)       { disp_write(">", 2,5,1); } 
+  else if (menu_item == 2)  { disp_write(">", 2,5,2); }
+}
+
+
+
+void setTimezone() {
+  // -----
+  // Changes the timezone from presaved values
+  // -----
+
+  // Contains all the standard timezones and their offsets
+  int timezone_times[]    = {-43200,-39600,-36000, -34200, -32400, -28800, -25200, -21600, -18000, -14400, -12600, -10800, -7200, -3600, 
+                             0, 3600, 7200, 10800, 12600, 14400, 16200, 18000, 19800, 20700, 21600, 23400, 25200, 28800, 31500, 32400, 
+                             34200, 36000, 37800, 39600, 43200, 45900, 46800, 50400};
+  String timezone_names[] = {" -12:00", " -11:00", " -10:00", "  -9:30", "  -9:00", "  -8:00", "  -7:00", "  -6:00", "  -5:00", "  -4:00", 
+                             "  -3:30", "  -3:00", "  -2:00", "  -1:00", "+/-0:00", "  +1:00", "  +2:00", "  +3:00", "  +3:30", "  +4:00", 
+                             "  +4:30", "  +5:00", "  +5:30", "  +5:45", "  +6:00", "  +6:30", "  +7:00", "  +8:00", "  +8:45", "  +9:00", 
+                             "  +9:30", " +10:00", " +10:30", " +11:00", " +12:00", " +12:45", " +13:00", " +14:00"};
+  
+  int menu_item = 1;
+  int button = 0;
+  int timezone = 14;
+
+  while (true) {
+    showTimezone(timezone_names[timezone], menu_item);
+    delay(refresh_rate);
+    button = checkButtonPress();
+
+    if (button == 2) {                                // Go UP
+      menu_item--;
+      if (menu_item<1) { menu_item = 2 - menu_item; }
+
+    } else if (button == 3) {                         // Go DOWN
+      menu_item++;
+      if (menu_item>2) { menu_item = menu_item - 2; }
+
+    } else if (button == 1) {
+      if (menu_item == 1) {                           // Incrementing timezone
+        timezone++;
+        if (timezone>37) { timezone = timezone - 38; }
+
+      } else {                                        // Saving and updating the NTP connection settings
+        UTC_OFFSET = timezone_times[timezone];
+        configTime(UTC_OFFSET, UTC_OFFSET_DST, NTP_SERVER);
+        break;
+      }
+    }
+  }
+}
+
+
+
+void settings() {
+  int menu_item = 1;
+  int button = 0;
+
+  while (true) {
+    showSettings(menu_item);           // Increment
+    delay(refresh_rate);
+    button = checkButtonPress();
+
+    if (button==1) {
+      menu_item = (menu_item==0) ? 4 : menu_item-1;
+      disp_cls();
+
+    } else if (button==3) {           // Decrement
+      menu_item = (menu_item==5) ? 1 : menu_item+1;
+      disp_cls();
+
+    } else if (button==2) {           // Select
+      if (menu_item==1) {
+        disp_cls();
+        connectWiFi();
+      } else if (menu_item==2) {
+        disp_cls();
+        chooseCalls();
+      } else if (menu_item==3) {
+        disp_cls();
+        setTimezone();
+      } else {
+        break;
+      }
+    }
+  }
+}
+
+
 
 void setup() {
   // WiFi.mode(WIFI_STA); // explicitly set mode, esp defaults to STA+AP
@@ -211,8 +420,19 @@ void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
   wifiSetup();
+
+  //Getting NTP time
+
 }
 
+
+
 void loop() {
+  showMainMenu();
+  
+  if (checkButtonPress() == 2) {
+    settings();
+  }
   wifiLoop();
+  //callPress();
 }
